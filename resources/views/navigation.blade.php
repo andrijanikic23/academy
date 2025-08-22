1aba2c0b30292337fd384b5ba6645b5b372c414c
andrijanikic23 <andrijanikic001@gmail.com>
2025-08-22 16:16:36 +0200

    * Tennis school page and navigation

--- resources/views/navigation.blade.php
+++ resources/views/navigation.blade.php
@@ -7,18 +7,18 @@
     <div class="collapse navbar-collapse" id="navbarNav">
         <ul class="navbar-nav">
             <li class="nav-item active">
-                <a class="nav-link" href="{{ route('welcome') }}">Početna <span class="sr-only">(current)</span></a>
+                <a class="nav-link active" href="{{ route('welcome') }}">Početna <span class="sr-only">(current)</span></a>
             </li>
-            <li class="nav-item">
+            <li class="nav-item  active">
                 <a class="nav-link" href="#">O nama</a>
             </li>
-            <li class="nav-item">
+            <li class="nav-item  active">
                 <a class="nav-link" href="{{ route('session.schedule') }}">Rezervacija termina</a>
             </li>
-            <li class="nav-item">
+            <li class="nav-item  active">
                 <a class="nav-link" href="#">Treneri</a>
             </li>
-            <li class="nav-item">
+            <li class="nav-item  active">
                 <a class="nav-link" href="{{ route('school') }}">Škola tenisa</a>
             </li>
         </ul>
---
+++ resources/views/tennisSchool.blade.php
@@ -0,0 +1,33 @@
+@extends("layout")
+
+
+@section("content")
+    <article class="container d-flex flex-column gap-5">
+        <div class="d-flex flex-row gap-5">
+            <img src="https://tennisacademyelite.com/wp-content/uploads/2020/09/junior-program-720x460.jpg" alt="Kids from tennis school">
+            <div class="d-flex flex-column justify-content-center align-items-center">
+                <h3>Škola tenisa</h3>
+                <p>Kroz školu tenisa TK Baseline Warriors prošlo je preko 2000 dece.</p>
+            </div>
+        </div>
+        <div class="d-flex flex-row gap-5">
+            <div class="d-flex flex-column justify-content-center align-items-center">
+                <h3>Takmičarski pogon</h3>
+                <p>U klubu je do sada organizovano preko 200 turnira sa preko 4000 dece od 5 do 18 godina.</p>
+            </div>
+            <img src="https://cagneytennisacademy.com.au/wp-content/uploads/2023/12/b-bn-84965hb7727.jpg" style="width:720px; height:460px;" alt="Kid with his coach">
+        </div>
+        <div class="d-flex flex-column gap-5">
+            <div class="d-flex flex-row gap-2">
+                <img src="https://www.helenricetennis.com.au/wp-content/uploads/2019/06/Private-Tennis-Coaching.jpg" style="width:720px; height:460px;">
+                <img src="https://tkdjukic.rs/wp-content/uploads/2023/10/317A3959.jpg" style="width:720px; height:460px;">
+            </div>
+
+            <div class="d-flex flex-row gap-2">
+                <img src="https://www.sherfieldschool.co.uk/userfiles/images/SHERFIELD_020724_tomsoperphotography_97.jpg" style="width:720px; height:460px;">
+                <img src="https://aceify.me/the-ace/wp-content/uploads/2022/11/TAimage1.png" style="width:720px; height:460px;">
+            </div>
+            <
+        </div>
+    </article>
+@endsection
