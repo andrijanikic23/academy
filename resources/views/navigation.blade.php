99993832b7187440959fdf9b86353ebcac4edcb5
andrijanikic23 <andrijanikic001@gmail.com>
2025-08-22 14:02:50 +0200

    * Welcome page beginning

---
+++ resources/views/navigation.blade.php
@@ -0,0 +1,23 @@
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <nav class="navbar navbar-light bg-light">
+        <a class="navbar-brand" href="{{ route('welcome') }}">
+            <img src="images/logo.png" width="60" height="60" alt="">
+        </a>
+    </nav>
+    <div class="collapse navbar-collapse" id="navbarNav">
+        <ul class="navbar-nav">
+            <li class="nav-item active">
+                <a class="nav-link" href="{{ route('welcome') }}">Početna <span class="sr-only">(current)</span></a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">O nama</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="{{ route('session.schedule') }}">Rezervacija termina</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">Treneri</a>
+            </li>
+        </ul>
+    </div>
+</nav>
--- routes/web.php
+++ routes/web.php
@@ -4,9 +4,7 @@ use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\SlotsController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::view("/", "welcome")->name("welcome");
 
 Route::get('/dashboard', function () {
     return view('dashboard');
