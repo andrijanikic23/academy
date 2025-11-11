10f4ac7eaa24cd9edf72d9ef33353443218f3fbe
andrijanikic23 <andrijanikic001@gmail.com>
2025-11-11 20:21:17 +0100

    * Changing the structure of footer part I

--- resources/views/footer.blade.php
+++ resources/views/footer.blade.php
@@ -1,3 +1,37 @@
 <footer class="bg-dark text-white text-center py-3 mt-auto">
+    <div class="conatiner">
+        <div class="row">
+            <div>
+                <h3>O nama</h3>
+
+                <p>Teniski klub “Baseline Warriors” osnovan je 1996. godine u Beogradu. Kroz 20 godina postojanja, klub je do sada sa velikim uspehom iznedrio mnoštvo domaćih igrača i igračica, od kojih je većina postigla zapažene juniorske i seniorske uspehe u svetu tenisa poput Miomira Kecmanovića, Filipa Krajinovića, Dušana Lajovića i drugih.</p>
+                <a>Pročitaj više</a>
+            </div>
+
+            <div>
+                <h3>O nama</h3>
+
+                <p>Teniski klub “Baseline Warriors” osnovan je 1996. godine u Beogradu. Kroz 20 godina postojanja, klub je do sada sa velikim uspehom iznedrio mnoštvo domaćih igrača i igračica, od kojih je većina postigla zapažene juniorske i seniorske uspehe u svetu tenisa poput Miomira Kecmanovića, Filipa Krajinovića, Dušana Lajovića i drugih.</p>
+                <a>Pročitaj više</a>
+            </div>
+
+            <div>
+                <h3>O nama</h3>
+
+                <p>Teniski klub “Baseline Warriors” osnovan je 1996. godine u Beogradu. Kroz 20 godina postojanja, klub je do sada sa velikim uspehom iznedrio mnoštvo domaćih igrača i igračica, od kojih je većina postigla zapažene juniorske i seniorske uspehe u svetu tenisa poput Miomira Kecmanovića, Filipa Krajinovića, Dušana Lajovića i drugih.</p>
+                <a>Pročitaj više</a>
+            </div>
+
+            <div>
+                <h3>O nama</h3>
+
+                <p>Teniski klub “Baseline Warriors” osnovan je 1996. godine u Beogradu. Kroz 20 godina postojanja, klub je do sada sa velikim uspehom iznedrio mnoštvo domaćih igrača i igračica, od kojih je većina postigla zapažene juniorske i seniorske uspehe u svetu tenisa poput Miomira Kecmanovića, Filipa Krajinovića, Dušana Lajovića i drugih.</p>
+                <a>Pročitaj više</a>
+            </div>
+        </div>
+    </div>
     &copy; 2025 Baseline Warriors. All rights reserved.
+
+
+
 </footer>
